c10fb570f585beb9de114607a450bfbff6e39cbc
thevgavini <77669880+thevgavini@users.noreply.github.com>
2023-04-03 12:30:47 +0530

    Formatted

--- QuickSortAnalysis.py
+++ QuickSortAnalysis.py
@@ -1,51 +1,46 @@
 import random
 import matplotlib.pyplot as plt
 from timeit import default_timer as timer
-z=[k for k in range(0,100,10)]
-zt=[]
+z = [k for k in range(0, 100, 10)]
+zt = []
 
 for n in z:
-  start = timer()
-  
-  
-  def partition(array, low, high):
-    pivot = array[high]
-    i = low - 1
-    for j in range(low, high):
-      if array[j] <= pivot:
-        i = i + 1
-        (array[i], array[j]) = (array[j], array[i])
+    start = timer()
 
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    def partition(array, low, high):
+        pivot = array[high]
+        i = low - 1
+        for j in range(low, high):
+            if array[j] <= pivot:
+                i = i + 1
+                (array[i], array[j]) = (array[j], array[i])
 
-    return i + 1
-    
-  def quickSort(array, low, high):
-    if low < high:
+        (array[i + 1], array[high]) = (array[high], array[i + 1])
 
-      pi = partition(array, low, high)
+        return i + 1
 
-      quickSort(array, low, pi - 1)
+    def quickSort(array, low, high):
+        if low < high:
 
-      quickSort(array, pi + 1, high)
-  
-  
-  data = []
-  for a in range(0,n):
-    data.append(random.randint(1, 100))
-  
+            pi = partition(array, low, high)
 
-  
-  size = len(data)
-  
-  quickSort(data, 0, size - 1)
-  
+            quickSort(array, low, pi - 1)
 
-  end = timer()
-  zt.append(end-start)
+            quickSort(array, pi + 1, high)
+
+    data = []
+    for a in range(0, n):
+        data.append(random.randint(1, 100))
+
+    size = len(data)
+
+    quickSort(data, 0, size - 1)
+
+    end = timer()
+    zt.append(end-start)
 
 
-plt.plot(z,zt)
+plt.plot(z, zt)
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title("Time analysis of Quick Sort")
